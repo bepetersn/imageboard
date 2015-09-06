684e9d53cd977c2137578e52da74051432f936a8
Brian Peterson <bepetersn@gmail.com>
2015-09-06 13:22:38 -0600

    Make migrations

--- manage.py
+++ manage.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
 from flask_script import Manager
-from imageboard import app
+from flask_migrate import Migrate, MigrateCommand
+from imageboard import app, db
+
 
 if __name__ == '__main__':
     manager = Manager(app)
+    migrate = Migrate(app, db)
+    manager.add_command('db', MigrateCommand)
     manager.run(default_command='runserver')
+
